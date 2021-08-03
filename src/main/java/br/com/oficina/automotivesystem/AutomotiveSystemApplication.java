a7df1bb66d74b2ab4b3f94964120b325cc7352d1
Israel Oliveira <israelisaque@hotmail.com>
2021-08-02 22:32:21 -0300

    Initial commit

---
+++ src/main/java/br/com/oficina/automotivesystem/AutomotiveSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.oficina.automotivesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveSystemApplication.class, args);
+	}
+
+}
